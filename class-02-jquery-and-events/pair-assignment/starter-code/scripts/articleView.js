5f3c41060c7834152c4810df69c0a2b291294a4b
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-04-12 16:17:19 -0700

    finished two more to dos

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -32,11 +32,15 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
-  //       When an option with a value is selected, hide all the articles, then reveal the matches.
-  //       When the blank (default) option is selected, show all the articles, except for the template.
-  //       Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function(){
+    if ($(this).val()) {
+      $('article').hide();
+      $('article').filter('article[data-category = "' + $(this).val() + '"]').fadeIn(600);
+    } else {
+      $('article').filter(!'article[class="template"]').fadeIn(600);
+    }
+    $('#author-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function() {
@@ -45,7 +49,17 @@ articleView.handleMainNav = function() {
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-  $('.main-nav').on(/* CODE GOES HERE */);
+  $('.main-nav').on('click', '.tab', function(e) {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+    // e.preventDefault();
+    // var $clickedTab = $(this).val();
+    // console.log($clickedTab);
+    // $('').hide($clickedTab);
+
+    // $('li').show($clickedTab);
+    // $(this).append(clicked);
+  });
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
@@ -65,4 +79,6 @@ articleView.setTeasers = function() {
 $(function(){
   articleView.populateFilters();
   articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
 });
