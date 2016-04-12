7ee48499343c0af7eeee616ddb31935b7cf1f1be
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-04-12 16:54:22 -0700

    finished last todos: stlyed also

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -44,35 +44,27 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav element that will power the Tabs feature.
-  //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
-  //       single .tab-content section that is associated with the clicked .tab element.
-  //       So: You need to dynamically build a selector string with the correct ID, based on the
-  //       data available to you on the .tab element that was clicked.
   $('.main-nav').on('click', '.tab', function(e) {
     $('.tab-content').hide();
     $('#' + $(this).data('content')).fadeIn();
-    // e.preventDefault();
-    // var $clickedTab = $(this).val();
-    // console.log($clickedTab);
-    // $('').hide($clickedTab);
-
-    // $('li').show($clickedTab);
-    // $(this).append(clicked);
   });
-
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
 articleView.setTeasers = function() {
   $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
-
+  $('article').on('click', 'a', function(e){
+    e.preventDefault();
+    $(this).hide();
+    $('.article-body *:nth-of-type(n+2)').fadeIn();
+  });
   // TODO: Add an event handler to reveal all the hidden elements,
   //       when the .read-on link is clicked. You can go ahead and hide the
-  //       "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  //       "Read On" link once it has been clicked. Be sure to p`revent the default link-click action!
   //       Ideally, we'd attach this as just 1 event handler on the #articles section, and let it
   //       process any .read-on clicks that happen within child nodes.
 // add a read less link
+  // $('article a').click();
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
@@ -81,4 +73,5 @@ $(function(){
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 });
