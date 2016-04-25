e1cb68726515bfd395366a4242aba89c50189298
apalm112 <apalm112@gmail.com>
2016-04-25 14:42:26 -0700

    Moved Article.fetchAll() into the adminController.js & hid the #about section for the admin page. Met the Stretch Goal!

--- class-11-spa-routing/pair-assignment/starter-code/scripts/adminController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/adminController.js
@@ -2,8 +2,12 @@
   var adminController = {};
 
   adminController.index = function() {
+
+    Article.fetchAll(articleView.initAdminPage);
     $('main > section').hide();
+    $('#about').hide();
     $('#admin').show();
+
   };
 
   module.adminController = adminController;
