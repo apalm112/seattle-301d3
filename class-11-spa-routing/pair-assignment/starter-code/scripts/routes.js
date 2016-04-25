e708eef0f837bed47b3e4576fa21257af27adaa7
apalm112 <apalm112@gmail.com>
2016-04-25 14:26:15 -0700

    Working on stretch goal, added adminController.js file.

--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -2,6 +2,7 @@
 //  each URL your app can handle, linked to a single controller function to handle it:
 page('/', articlesController.index);
 page('/about', aboutController.index);
+page('/admin', adminController.index);
 
 
 // done What function do we call to activate page.js? Fire it off now, to execute it:
